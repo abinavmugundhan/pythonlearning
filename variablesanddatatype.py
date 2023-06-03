9f39f9a3215aae86952de67d89c91b6f24fa3c86
abinavmugundhan <135412989+abinavmugundhan@users.noreply.github.com>
2023-06-03 12:14:05 +0530

    variable and datatype

---
+++ variablesanddatatype.py
@@ -0,0 +1,27 @@
+#type() predefined function is used to predict what type value assign to variable
+##a=10#integer #int
+##print(type(a))
+##a=10.5
+##print(type(a))
+##a="lettrer"
+##print(type(a))
+##a=True
+##print(type(a))
+##a=[1,2,3,4]
+##print(type(a))
+##a=(1,2,3,4)
+##print(type(a))
+##a={1,2,3,4}
+##print(type(a))
+##a={1:"one",2:"two"}
+##print(type(a))
+##name="divya"
+##age=22
+##print("My name is",name,". I am",age,"years old.")
+a="ocean"
+b="academy"
+print(a,".",b)
+print(a+". "+b)
+a="string"
+b=9
+print(a+b)
