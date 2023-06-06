8ea99a9aa4c144788673d6755a039a3006efb0a8
abinavmugundhan <135412989+abinavmugundhan@users.noreply.github.com>
2023-06-06 10:53:41 +0530

    Add files via upload
    LEAVE LETTER

---
+++ demo 3.py
@@ -0,0 +1,15 @@
+name="MUGUNDHAN"
+age=20
+address="PONDICHERRY"
+mobile=9384286425
+email="abinavmugundhan011@gmail.com"
+
+print("----------------------------------------------------------------------------------------")
+print("\t\tSTUDENT INFORMATION")
+print("----------------------------------------------------------------------------------------")
+print("\t\tNAME        : ",name)
+print("\t\tAGE          : ",age)
+print("\t\tADDRESS : ",address)
+print("\t\tMOBILE    : ",mobile)
+print("\t\tEMAIL       : ",email)
+print("-------------------------------------------------------------------------------------")
