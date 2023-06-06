6b1d5ad0a297f6f303eefe303aff9cc46d4af988
abinavmugundhan <135412989+abinavmugundhan@users.noreply.github.com>
2023-06-06 11:02:42 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,19 @@
+print("FROM")
+print("\t\tS.MUGUNDHAN,")
+print("\t\tNO.10,11,ILA APARTMENT,")
+print("\t\tCOLAS NAGAR,")
+print("\t\tPONDICHEERY-605001.")
+
+print("TO")
+print("\t\tTHE PRINCIPAL,")
+print("\t\tSRIRAM SCHOOL,")
+print("\t\tPONDICHERRY-605001.")
+      
+print("RESPECTED SIR,")
+print("\t\tAs I am suffering from fever, I am unable to attend")
+print("the class. so kindly grant me leave for two days.")
+
+print("\t\t\tTHANKING YOU")
+
+print("place: PONDICHERRY                       Yours faithfully,")
+print("Date:02.06.2023                           S.MUGUNDHAN")
